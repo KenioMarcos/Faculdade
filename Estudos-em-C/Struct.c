8d01fdb035d369257c408c35b62b0c2e3b3dc2cb
KenioMarcos <marcos@keniomarcos.com>
2021-08-24 09:37:52 -0400

    Create Struct.c

---
+++ Estudos-em-C/Struct.c
@@ -0,0 +1,30 @@
+/* modelo de tipos de dados estruturados */
+
+#include <stdio.h>
+
+struct data
+{
+    int mes;
+    int ano;
+};
+
+struct livro
+{
+    char nome[30];
+    char autor[50];
+    struct data data_pub;
+};
+
+int main()
+{
+    struct livro l1 = {"Primo Basílio", "Eça de Queiroz"};
+    l1.data_pub.ano = 1878;
+    struct livro l2 = {"Dom Casmurro", "Machado de Assis"};
+    l2.data_pub.ano = 1899;
+    
+    printf("Livro 1: %s - Autor: %s - Ano: %d\n", l1.nome, l1.autor, l1.data_pub.ano);
+    printf("Livro 2: %s - Autor: %s - Ano: %d\n", l2.nome, l2.autor, l2.data_pub.ano);
+        
+    printf("\n\n");
+    return 0;
+}
