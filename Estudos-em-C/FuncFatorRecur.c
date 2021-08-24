4d247221058868e0c602fb828f04ccfe17907768
KenioMarcos <marcos@keniomarcos.com>
2021-08-24 05:31:14 -0400

    Create FuncFatorRecur.c

---
+++ Estudos-em-C/FuncFatorRecur.c
@@ -0,0 +1,25 @@
+/* Cria uma função para fatorar um número qualquer, depois essa função é chamada
+no main para fatorar um dado número */
+
+#include <stdio.h>
+
+int fatorial(int n)
+{
+    if(n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * fatorial(n - 1);
+    }
+}
+
+int main()
+{
+    int resultado;
+    resultado = fatorial(10); // dentro do parênteses você insere o número que quer fatorar
+    printf("fatorial de 5 é: %d\n\n", resultado);
+    
+    return 0;
+}
