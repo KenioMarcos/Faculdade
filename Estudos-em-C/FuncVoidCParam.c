4b1a5917b560bb5628c986c083004da234cb19f3
KenioMarcos <marcos@keniomarcos.com>
2021-08-24 05:55:45 -0400

    Create FuncVoidCParam.c

---
+++ Estudos-em-C/FuncVoidCParam.c
@@ -0,0 +1,32 @@
+/* Cria uma função void para imprimir na tela uma linha com n símbolos */
+
+#include <stdio.h>
+
+void linha(char c, int count)
+{
+    int i;
+    
+    for (size_t i = 0; i <= count; i++)
+    {
+        printf("%c", c);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int i;
+    int count = 10;
+        
+    linha('#', 30);
+    printf("números de 0 a 10:\n");
+    for (size_t i = 0; i <= count; i++)
+    {
+        printf("%d", i);
+    }
+    printf("\n");
+    linha('#', 30);
+    printf("\n");
+    
+    return 0;
+}
