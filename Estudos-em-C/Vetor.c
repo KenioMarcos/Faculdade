bcd3f07e3dc9ec9f237f8326a88fabc7091d8e99
KenioMarcos <marcos@keniomarcos.com>
2021-08-24 09:11:19 -0400

    Create Vetor.c

---
+++ Estudos-em-C/Vetor.c
@@ -0,0 +1,17 @@
+/* inicialização de um vetor, também conhecido por array */
+
+#include <stdio.h>
+
+#define MAX 5
+
+int main()
+{
+    int i, idades [MAX] = {25, 21, 32,40, 69};
+
+    for (size_t i = 0; i < MAX; i++)
+    {
+        printf("%3d", idades[i]);
+    }
+    printf("\n\n");
+    return 0;
+}
